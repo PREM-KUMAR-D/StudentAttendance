583531dc1bf0e7df09e9319cb8ee1c33470dc6d4
PREM-KUMAR-D <33512522+PREM-KUMAR-D@users.noreply.github.com>
2024-06-14 19:56:51 +0530

    Delete backend/util directory

--- backend/util/database.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require('sequelize')
-
-const sequelize = new Sequelize('student-attendance','root','MPasssword@123',{
-    dialect:'mysql',host:'localhost'
-});
-
-module.exports = sequelize;
